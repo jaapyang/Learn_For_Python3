cc6bbd2505d0d5ba1a8c21b92a2038e820867a84
jaapyang <15873115257@qq.com>
2017-06-21 21:53:55 +0800

    draw arc

--- src/Demos/mypolygon.py
+++ src/Demos/mypolygon.py
@@ -21,10 +21,20 @@ def polygon(t, n, length):
     polyline(t, n, length, angle)
 
 
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    print(arc_length)
+    n = int(arc_length / 4) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    t.lt(step_angle / 2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle / 2)
+
+
 if __name__ == '__main__':
     bob = turtle.Turtle()
-    polygon(bob, 360, 1)
-    bob.pu()
+    arc(bob, 100, 360)
 
     # wait for the user to close the window
     turtle.mainloop()
