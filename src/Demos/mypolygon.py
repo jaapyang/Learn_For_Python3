644ab4067f501ae306465e4947f8ddcaa0916be1
jaapyang <15873115257@qq.com>
2017-06-21 20:58:20 +0800

    learn for use turtle

---
+++ src/Demos/mypolygon.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import turtle
+import math
+
+def polyline(t, n, length, angle):
+    """Draws n line segments.
+
+    t: Turtle object
+    n: number of line segments
+    length: length of each segment
+    angle: degrees between segments
+    """
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    t.lt(step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
+
+def circle(t, r):
+    """Draws a circle with the given radius.
+
+    t: Turtle
+    r: radius
+    """
+    arc(t, r, 360)
+
+if __name__ == '__main__':
+    bob = turtle.Turtle()
+
+    # draw a circle centered on the origin
+    radius = 100
+    bob.pu()
+    bob.fd(radius)
+    bob.lt(90)
+    bob.pd()
+    circle(bob, radius)
+
+    # wait for the user to close the window
+    turtle.mainloop()
